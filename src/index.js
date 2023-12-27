6fe50cdd082640b22a265263faa33a7af62bbc4b
MOHD NAZIR ANWER <122814116+naziranwer@users.noreply.github.com>
2023-12-27 23:48:09 +0530

    readfile

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ const fileName = "myfile.txt";
 const reader = async (fileName) => {
   // write code here
 	// dont change function name
+  try{
+      const data= fs.readFile(fileName,{encoding :'utf8'});
+      return data;
+  }
+  catch(e){
+       console.error(e);
+  }
   
 };
 
